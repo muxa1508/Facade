952338dd5e0ec199b6d9120d651e85b52e49ed81
muxa1508 <muxa1508@inbox.ru>
2022-11-02 20:54:09 +0300

    Task 2 complete

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+
+        bins.sum("011101","000110");
+
+        System.out.println();
+
+        bins.mult("011101","000110");
+
+
+    }
+}
